6daa69eb6b339b50f8ae871d09d6839ae4b67b0d
gfernandez <gfernandez@uea.edu.ec>
2024-08-11 20:25:58 -0500

    Semana 09

---
+++ Unidad 03/3-1-1 -- arreglos/3.1.1-07.arreglo-clase.py
@@ -0,0 +1,15 @@
+student_grades = [
+    [85, 92, 78],
+    [90, 88, 95],
+    [75, 80, 85]
+]
+
+#modificar valor 80 por 42
+print(student_grades[2][1])
+student_grades[2][1] = 42
+print(student_grades[2][1])
+
+# modificar valor 85 por 0
+print(student_grades[2][2])
+student_grades[2][2] = 0
+print(student_grades[2][2])
